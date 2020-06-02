db0d5308203f7a979dcae05c4d2e89880a6ab29d
Mariya <mariya.podosinova@gmail.com>
2020-06-02 14:48:08 +0100

    Updates

---
+++ precache-manifest.f23caf7e90e121a2f6dab2c292689db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03db10ffcecf8cc4ca242b16d86fd676",
+    "url": "/cards_deck/index.html"
+  },
+  {
+    "revision": "c11ebcb8baa545bf176a",
+    "url": "/cards_deck/static/css/main.dabf94ce.chunk.css"
+  },
+  {
+    "revision": "11809500d8b2622cbee0",
+    "url": "/cards_deck/static/js/2.43464db9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cards_deck/static/js/2.43464db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11ebcb8baa545bf176a",
+    "url": "/cards_deck/static/js/main.5f329dd7.chunk.js"
+  },
+  {
+    "revision": "2ec41b4406b81773c595",
+    "url": "/cards_deck/static/js/runtime-main.e6584750.js"
+  }
+]);
